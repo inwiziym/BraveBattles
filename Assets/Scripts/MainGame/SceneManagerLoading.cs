b802f8cc95111f8509b4f66344cd770985994f9f
inwiziym <zadorozhnydenis@mail.ru>
2023-05-07 11:44:51 +0700

    0.0.16
    Little fix

--- Assets/Scripts/MainGame/SceneManagerLoading.cs
+++ Assets/Scripts/MainGame/SceneManagerLoading.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerLoading : MonoBehaviour
 {
     public int indexScene;
     public int[] randomScene;
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
     public void LoadSceneIndex()
     {
         SceneManager.LoadScene(indexScene);
@@ -20,6 +26,23 @@ public class SceneManagerLoading : MonoBehaviour
     public void LoadRandomScene()
     {
         int randomIndex = Random.Range(0, randomScene.Length);
-        SceneManager.LoadScene(randomScene[randomIndex]);
+        StartCoroutine(LoadAsync(randomScene[randomIndex]));
+        Time.timeScale = 0;
+    }
+
+    IEnumerator LoadAsync(int randomIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(randomIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            progressText.text = Mathf.RoundToInt(progress * 100) + "%";
+
+            yield return null;
+        }
+        loadingScreen.SetActive(false);
     }
 }
--- Assets/Scripts/MainGame/SettingsLevel.cs
+++ Assets/Scripts/MainGame/SettingsLevel.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-
 public class SettingsLevel : MonoBehaviour
 {
     public void Start()
